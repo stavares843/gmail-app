7cfd94af69522b169daf5477311e6e69b9a98b25
stavares843 <saratavares843@gmail.com>
2025-11-01 23:40:46 +0000

    fix build

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,6 +1,6 @@
 import app from './app';
 
-const port = process.env.PORT || 4000;
-app.listen(port, () => {
-  console.log(`API listening on http://localhost:${port}`);
+const port = Number(process.env.PORT) || 4000;
+app.listen(port, '0.0.0.0', () => {
+  console.log(`API listening on http://0.0.0.0:${port}`);
 });
